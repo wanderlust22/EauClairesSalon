c2dab0736ba8745049e8187bbcafa48d75a7a528
Student <example@example.com>
2020-01-17 13:16:53 -0800

    add controllers and views and functionality to edit a client in a stylits roster

--- HairSalon/Controllers/ClientController.cs
+++ HairSalon/Controllers/ClientController.cs
@@ -3,6 +3,7 @@ using Salon.Models;
 using System.Linq;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace Salon.Controllers
 {
@@ -22,5 +23,46 @@ namespace Salon.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Create(Client client)
+        {
+            _db.Client.Add(client);
+            _db.SaveChanges();
+            return RedirectToAction("Details", "Stylist", new { id = client.StylistId});
+        }
+
+        public ActionResult Details(int id)
+        {
+            Client model = _db.Client.FirstOrDefault(client => client.ClientId == id);
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Client model = _db.Client.FirstOrDefault(client => client.ClientId == id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Client theClient)
+        {
+            _db.Entry(theClient).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Details", "Stylist", new { id = theClient.StylistId});
+        }
+        
+        public ActionResult Delete(int id)
+        {
+            Client model = _db.Client.FirstOrDefault(client => client.ClientId == id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Client theClient)
+        {
+            _db.Client.Remove(theClient);
+            _db.SaveChanges();
+            return RedirectToAction("Details", "Stylist", new { id = theClient.StylistId});
+        }
     }
 }
